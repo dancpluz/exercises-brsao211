565255dea961639841828550ab4b96c172bf8053
dancpluz <dan08jan@gmail.com>
2026-01-12 16:47:10 -0300

    feat: atividade 1 - questoes de 1 a 4

--- atividadepratica01/questao01.py
+++ atividadepratica01/questao01.py
@@ -4,3 +4,5 @@
 * Crie um programa que imprima a mensagem "Olá, mundo!" na tela.
 
 """
+
+print("Olá, mundo!")
--- atividadepratica01/questao03.py
+++ atividadepratica01/questao03.py
@@ -9,3 +9,9 @@
 O programa deve calcular o volume e exibir o resultado em cm³.
 
 """
+
+comprimento = 12
+largura = 14
+altura = 20
+volume = comprimento * largura * altura
+print(f"O volume da caixa retangular é: {volume} cm³")
