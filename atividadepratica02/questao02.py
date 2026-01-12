564c701ac8a314954f34fc0e3ecd440cf739c99e
dancpluz <dan08jan@gmail.com>
2026-01-12 16:56:48 -0300

    feat: atividade 2 questao 2

--- atividadepratica02/questao02.py
+++ atividadepratica02/questao02.py
@@ -9,3 +9,16 @@ Desenvolva um programa que calcula o desconto em uma loja. Use as seguintes info
 O programa deve calcular o valor do desconto e o preço final, exibindo todos os detalhes.
 
 """
+
+nome_produto = input("Digite o nome do produto: ")
+preco_original = float(input("Digite o preço unitário do produto (R$): "))
+porcentagem_desconto = float(input("Digite a porcentagem de desconto (%): "))
+
+valor_desconto = (porcentagem_desconto / 100) * preco_original
+preco_final = preco_original - valor_desconto
+
+print(f"\nProduto: {nome_produto}")
+print(f"Preço original: R$ {preco_original:.2f}")
+print(f"Porcentagem de desconto: {porcentagem_desconto}%")
+print(f"Valor do desconto: R$ {valor_desconto:.2f}")
+print(f"Preço final: R$ {preco_final:.2f}")
