59cbad0b2f1a7781a4c4e04913d2f926512fdc22
dancpluz <dan08jan@gmail.com>
2026-01-12 16:59:10 -0300

    feat: atividade 2 questao 3

--- atividadepratica02/questao01.py
+++ atividadepratica02/questao01.py
@@ -18,6 +18,6 @@ taxa_euro = 6.15
 valor_dolar = valor_reais / taxa_dolar
 valor_euro = valor_reais / taxa_euro
 
-print(f"Valor em reais: R$ {valor_reais:.2f}")
+print(f"\nValor em reais: R$ {valor_reais:.2f}")
 print(f"Valor em dólares: $ {valor_dolar:.2f}")
 print(f"Valor em euros: € {valor_euro:.2f}")
--- atividadepratica02/questao03.py
+++ atividadepratica02/questao03.py
@@ -9,3 +9,13 @@ Crie um programa que calcula a média escolar de um aluno. Use as seguintes nota
 O programa deve calcular a média e exibir todas as notas e o resultado final, arredondando para duas casas decimais.
 
 """
+
+nota1 = float(input("Digite a primeira nota: "))
+nota2 = float(input("Digite a segunda nota: "))
+nota3 = float(input("Digite a terceira nota: "))
+
+media = (nota1 + nota2 + nota3) / 3
+print(f"\nNota 1: {nota1:.2f}")
+print(f"Nota 2: {nota2:.2f}")
+print(f"Nota 3: {nota3:.2f}")
+print(f"Média final: {media:.2f}")
