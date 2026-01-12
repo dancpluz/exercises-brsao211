d39bc6c027805275eac03e84dbbbd1010c98b8a5
dancpluz <dan08jan@gmail.com>
2026-01-12 17:02:27 -0300

    feat: atividade 2 questao 4

--- atividadepratica02/questao04.py
+++ atividadepratica02/questao04.py
@@ -8,3 +8,12 @@ Desenvolva um programa que calcula o consumo médio de combustível de um veícu
 O programa deve calcular o consumo médio (km/l) e exibir todos os dados da viagem, incluindo o resultado final arredondado para duas casas decimais.
 
 """
+
+distancia_percorrida = float(input("Digite a distância percorrida (km): "))  # em km
+combustivel_gasto = float(input("Digite o combustível gasto (litros): "))  # em litros
+
+consumo_medio = distancia_percorrida / combustivel_gasto
+
+print(f"\nDistância percorrida: {distancia_percorrida} km")
+print(f"Combustível gasto: {combustivel_gasto} L")
+print(f"Consumo médio: {consumo_medio:.2f} km/l")
