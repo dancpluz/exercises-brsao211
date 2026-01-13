64f7da69fba42afa9937653356a6dc13535c091e
dancpluz <dan08jan@gmail.com>
2026-01-12 21:19:52 -0300

    feat: atividade 3 questao 4

--- atividadepratica03/questao04.py
+++ atividadepratica03/questao04.py
@@ -5,3 +5,12 @@ Faça um programa que determine se um ano inserido pelo usuário é bissexto ou 
 Um ano é bissexto se for divisível por 4, exceto anos centenários (divisíveis por 100) que não são divisíveis por 400.
 
 """
+
+ano = int(input("Digite um ano: "))
+
+if (ano % 4 == 0 and ano % 100 != 0) or (ano % 400 == 0):
+    resultado = "é bissexto"
+else:
+    resultado = "não é bissexto"
+
+print(f"\nO ano {ano} {resultado}.")
