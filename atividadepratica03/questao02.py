e126342616f79c069b93f099019969e82774dbfa
dancpluz <dan08jan@gmail.com>
2026-01-12 21:18:27 -0300

    feat: atividade 3 questao 3

--- atividadepratica03/questao02.py
+++ atividadepratica03/questao02.py
@@ -16,6 +16,7 @@ peso = float(input("Digite seu peso em kg: "))
 altura = float(input("Digite sua altura em metros: "))
 
 imc = peso / (altura ** 2)
+
 if imc < 18.5:
     classificacao = "Abaixo do peso"
 elif imc < 25:
--- atividadepratica03/questao03.py
+++ atividadepratica03/questao03.py
@@ -5,3 +5,40 @@ Crie um programa que converta temperaturas entre Celsius, Fahrenheit e Kelvin.
 O usuário deve informar a temperatura, a unidade de origem e a unidade para qual deseja converter.
 
 """
+
+def celsius_to_fahrenheit(c):
+    return (c * 9/5) + 32
+def celsius_to_kelvin(c):
+    return c + 273.15
+def fahrenheit_to_celsius(f):
+    return (f - 32) * 5/9
+def fahrenheit_to_kelvin(f):
+    return (f - 32) * 5/9 + 273.15
+def kelvin_to_celsius(k):
+    return k - 273.15
+def kelvin_to_fahrenheit(k):
+    return (k - 273.15) * 9/5 + 32
+
+temp = float(input("Digite a temperatura: "))
+origem = input("Digite a unidade de origem (C, F, K): ").upper()
+destino = input("Digite a unidade de destino (C, F, K): ").upper()
+
+if origem == destino:
+    resultado = temp
+elif origem == "C" and destino == "F":
+    resultado = celsius_to_fahrenheit(temp)
+elif origem == "C" and destino == "K":
+    resultado = celsius_to_kelvin(temp)
+elif origem == "F" and destino == "C":
+    resultado = fahrenheit_to_celsius(temp)
+elif origem == "F" and destino == "K":
+    resultado = fahrenheit_to_kelvin(temp)
+elif origem == "K" and destino == "C":
+    resultado = kelvin_to_celsius(temp)
+elif origem == "K" and destino == "F":
+    resultado = kelvin_to_fahrenheit(temp)
+else:
+    print("Unidade inválida.")
+    exit()
+
+print(f"\n{temp}°{origem} é igual a {resultado:.2f}°{destino}")
